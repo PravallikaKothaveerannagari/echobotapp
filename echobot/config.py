d215a910a06b722b3cf5cc9590bb427f084f56cb
PravallikaKV <113497539+PravallikaKothaveerannagari@users.noreply.github.com>
2024-03-21 18:25:58 +0530

    Update config.py

--- echobot/config.py
+++ echobot/config.py
@@ -9,5 +9,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "90f65359-c512-40d3-a10e-b5b76e6a2399")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "NKZ8Q~hLIMVHuamNiKQ~AqWHo4Qtab9ne9qUnbgW")
+    APP_ID = os.environ.get("MicrosoftAppId", "")
+    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
